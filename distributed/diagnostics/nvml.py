4c365bff957b0f92dd91324399ac1fb0a8a74ebf
rjzamora <rzamora217@gmail.com>
2019-08-23 09:48:38 -0700

    debug changes

--- distributed/diagnostics/nvml.py
+++ distributed/diagnostics/nvml.py
@@ -1,12 +1,25 @@
 import pynvml
 
+# need_pynvml_init = True
+# count = None
+# handles = None
+
 pynvml.nvmlInit()
 count = pynvml.nvmlDeviceGetCount()
-
 handles = [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(count)]
+
+# def _initialize_pynvml():
+#     global need_pynvml_init, count, handles
+#     pynvml.nvmlInit()
+#     count = pynvml.nvmlDeviceGetCount()
+#     handles = [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(count)]
+#     need_pynvml_init = False
 
 
 def real_time():
+    # global need_pynvml_init, handles
+    # if need_pynvml_init:
+    #     _initialize_pynvml()
     return {
         "utilization": [pynvml.nvmlDeviceGetUtilizationRates(h).gpu for h in handles],
         "memory-used": [pynvml.nvmlDeviceGetMemoryInfo(h).used for h in handles],
@@ -14,6 +27,9 @@ def real_time():
 
 
 def one_time():
+    # global need_pynvml_init, handles
+    # if need_pynvml_init:
+    #     _initialize_pynvml()
     return {
         "memory-total": [pynvml.nvmlDeviceGetMemoryInfo(h).total for h in handles],
         "name": [pynvml.nvmlDeviceGetName(h).decode() for h in handles],
